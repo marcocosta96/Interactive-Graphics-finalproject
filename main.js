ec007165ea6c06f9fa69c0172b287024d221d648
Marco <costa.1691388@studenti.uniroma1.it>
2019-06-27 17:29:53 +0200

    Fix

--- main.js
+++ main.js
@@ -1,2 +1,47 @@
 
 // Vars
+const maps = {
+    sun: {
+        color: 'img/sunColorMap.jpg'
+    },
+    mercury: {
+        color: 'img/mercuryColorMap.jpg',
+        bump: 'img/mercuryBumpMap.jpg'
+    },
+    venus: {
+        color: 'img/venusColorMap.jpg',
+        bump: 'img/venusBumpMap.jpg'
+    },
+    earth: {
+        color: 'img/earthColorMap.jpg',
+        bump: 'img/earthBumpMap.jpg',
+        specular: 'img/earthSpecularMap.jpg'
+    },
+    moon: {
+        color: 'img/moonColorMap.jpg',
+        bump: 'img/moonBumpMap.jpg'
+    },
+    mars: {
+        color: 'img/marsColorMap.jpg',
+        bump: 'img/marsBumpMap.jpg',
+        normal: 'img/marsNormalMap.jpg'
+    },
+    jupiter: {
+        color: 'img/jupiterColorMap.jpg'
+    },
+    saturn: {
+        color: 'img/saturnColorMap.jpg',
+        ring: 'img/saturnRingColor'
+    },
+    uranus: {
+        color: 'img/uranusColorMap.jpg',
+        ring: 'img/uranusRingColor'
+    },
+    neptune: {
+        color: 'img/neptuneColorMap.jpg'
+    },
+    pluto: {
+        color: 'img/plutoColorMap.jpg',
+        bump: 'img/plutoBumpMap.jpg'
+    },
+};
