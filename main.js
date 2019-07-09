0c3179ad996b7dfe71b7743440bc2c3bf6d49664
Marco <costa.1691388@studenti.uniroma1.it>
2019-07-09 20:37:56 +0200

    Added planets info

--- main.js
+++ main.js
@@ -32,12 +32,18 @@ const trajectorySegments = 1024;
 const data = [];
 data[earthId] = {
     name: "Earth",
+    type: "Rochy",
     size: 1,
+    diameter: "12756 km",
+    averageDistanceFromSun: "1 AU (150M of km)",
+    density: "5.52 g/cm\u00B3",
+    gravity: "9.807 m/s\u00B2",
+    numberOfMoons: 1,
     distance: 50,
-    orbitRate: 365,
+    revolutionRate: 365,
     rotationRate: 24,
     equatorInclination: 23.45,
-    orbitalInclination: 0,
+    orbitInclination: 0,
     orbitCenter: earthSystemId,
     groupId: earthSystemId,
     color: 'img/earthColorMap.jpg',
@@ -53,6 +59,9 @@ data[sunId] = {
     size: data[earthId].size * 15,
     rotationRate: data[earthId].rotationRate * 25.38,
     equatorInclination: 7.4166,
+    diameter: "696000 km",
+    density: "1.41 g/cm\u00B3",
+    gravity: "274 m/s\u00B2",
     color: 'img/sunColorMap.jpg',
     glow: 'img/glow.png',
     icon: 'img/sun.png',
@@ -60,12 +69,18 @@ data[sunId] = {
 };
 data[mercuryId] = {
     name: "Mercury",
+    type: "Rochy",
+    diameter: "4880 km",
+    averageDistanceFromSun: "0.39 AU (58M of km)",
+    density: "5.44 g/cm\u00B3",
+    gravity: "3.7 m/s\u00B2",
+    numberOfMoons: 0,
     size: data[earthId].size * 0.382,
     distance: data[earthId].distance * 0.387,
-    orbitRate: data[earthId].orbitRate * 0.0219,
+    revolutionRate: data[earthId].revolutionRate * 0.0219,
     rotationRate: data[earthId].rotationRate * 58.785,
     equatorInclination: 0,
-    orbitalInclination: 7,
+    orbitInclination: 7,
     orbitCenter: solarSystemId,
     color: 'img/mercuryColorMap.jpg',
     bump: 'img/mercuryBumpMap.jpg',
@@ -74,12 +89,18 @@ data[mercuryId] = {
 };
 data[venusId] = {
     name: "Venus",
+    type: "Rochy",
+    diameter: "12104 km",
+    averageDistanceFromSun: "0.72 AU (108M of km)",
+    density: "5.2 g/cm\u00B3",
+    gravity: "8.87 m/s\u00B2",
+    numberOfMoons: 0,
     size: data[earthId].size * 0.949,
     distance: data[earthId].distance * 0.723,
-    orbitRate: data[earthId].orbitRate * 0.6152,
+    revolutionRate: data[earthId].revolutionRate * 0.6152,
     rotationRate: data[earthId].rotationRate * 243.69,
     equatorInclination: 178,
-    orbitalInclination: 3.4,
+    orbitInclination: 3.4,
     orbitCenter: solarSystemId,
     color: 'img/venusColorMap.jpg',
     bump: 'img/venusBumpMap.jpg',
@@ -88,12 +109,18 @@ data[venusId] = {
 };
 data[marsId] = {
     name: "Mars",
+    type: "Rochy",
+    diameter: "6787 km",
+    averageDistanceFromSun: "1.52 AU (228M of km)",
+    density: "3.93 g/cm\u00B3",
+    gravity: "3.711 m/s\u00B2",
+    numberOfMoons: 2,
     size: data[earthId].size * 0.532,
     distance: data[earthId].distance * 1.524,
-    orbitRate: data[earthId].orbitRate * 1.881,
+    revolutionRate: data[earthId].revolutionRate * 1.881,
     rotationRate: data[earthId].rotationRate * 1.02595675,
     equatorInclination: 23.9833,
-    orbitalInclination: 1.85,
+    orbitInclination: 1.85,
     orbitCenter: solarSystemId,
     color: 'img/marsColorMap.jpg',
     bump: 'img/marsBumpMap.jpg',
@@ -103,12 +130,18 @@ data[marsId] = {
 };
 data[jupiterId] = {
     name: "Jupiter",
+    type: "Gas giant",
+    diameter: "142800 km",
+    averageDistanceFromSun: "5.20 AU (778M of km)",
+    density: "1.3 g/cm\u00B3",
+    gravity: "24.97 m/s\u00B2",
+    numberOfMoons: 16,
     size: data[earthId].size * 11.19,
     distance: data[earthId].distance * 5.203,
-    orbitRate: data[earthId].orbitRate * 11.86,
+    revolutionRate: data[earthId].revolutionRate * 11.86,
     rotationRate: data[earthId].rotationRate * 0.41354,
     equatorInclination: 3.0833,
-    orbitalInclination: 1.3,
+    orbitInclination: 1.3,
     orbitCenter: solarSystemId,
     color: 'img/jupiterColorMap.jpg',
     icon: 'img/jupiter.png',
@@ -116,12 +149,18 @@ data[jupiterId] = {
 };
 data[saturnId] = {
     name: "Saturn",
+    type: "Gas giant",
+    diameter: "120000 km",
+    averageDistanceFromSun: "9.54 AU (1427M of km)",
+    density: "0.69 g/cm\u00B3",
+    gravity: "10.44 m/s\u00B2",
+    numberOfMoons: 18,
     size: data[earthId].size * 9.26,
     distance: data[earthId].distance * 9.537,
-    orbitRate: data[earthId].orbitRate * 29.45,
+    revolutionRate: data[earthId].revolutionRate * 29.45,
     rotationRate: data[earthId].rotationRate * 0.44401,
     equatorInclination: 26.7333,
-    orbitalInclination: 2.4833,
+    orbitInclination: 2.4833,
     ringSegments: 500,
     orbitCenter: solarSystemId,
     groupId: saturnSystemId,
@@ -136,12 +175,18 @@ data[saturnId].ringInnerDiameter = data[saturnId].size * 1.5;
 data[saturnId].ringOuterDiameter = data[saturnId].ringInnerDiameter * 1.5;
 data[uranusId] = {
     name: "Uranus",
+    type: "Ice gas",
+    diameter: "51800 km",
+    averageDistanceFromSun: "19.19 AU (2878M of km)",
+    density: "1.28 g/cm\u00B3",
+    gravity: "8.87 m/s\u00B2",
+    numberOfMoons: 15,
     size: data[earthId].size * 4.01,
     distance: data[earthId].distance * 19.191,
-    orbitRate: data[earthId].orbitRate * 84.02,
+    revolutionRate: data[earthId].revolutionRate * 84.02,
     rotationRate: data[earthId].rotationRate * 0.71833,
     equatorInclination: 98,
-    orbitalInclination: 0.7666,
+    orbitInclination: 0.7666,
     ringSegments: 500,
     orbitCenter: solarSystemId,
     groupId: uranusSystemId,
@@ -156,24 +201,36 @@ data[uranusId].ringInnerDiameter = data[uranusId].size * 1.5;
 data[uranusId].ringOuterDiameter = data[uranusId].ringInnerDiameter * 1.5;
 data[neptuneId] = {
     name: "Neptune",
+    type: "Ice gas",
+    diameter: "49500 km",
+    averageDistanceFromSun: "30.06 AU (4497M of km)",
+    density: "1.64 g/cm\u00B3",
+    gravity: "11.15 m/s\u00B2",
+    numberOfMoons: 8,
     size: data[earthId].size * 3.88,
     distance: data[earthId].distance * 30.069,
-    orbitRate: data[earthId].orbitRate * 164.79,
+    revolutionRate: data[earthId].revolutionRate * 164.79,
     rotationRate: data[earthId].rotationRate * 0.67125,
     equatorInclination: 28.8,
-    orbitalInclination: 1.7666,
+    orbitInclination: 1.7666,
     orbitCenter: solarSystemId,
     color: 'img/neptuneColorMap.jpg',
     icon: 'img/neptune.png'
 };
 data[plutoId] = {
     name: "Pluto",
+    type: "Rochy",
+    diameter: "6000 km",
+    averageDistanceFromSun: "39.44 AU (5900M of km)",
+    density: "2.06 g/cm\u00B3",
+    gravity: "0.62 m/s\u00B2",
+    numberOfMoons: 1,
     size: data[earthId].size * 0.18,
     distance: data[earthId].distance * 39.44,
-    orbitRate: data[earthId].orbitRate * 249.6913,
+    revolutionRate: data[earthId].revolutionRate * 249.6913,
     rotationRate: data[earthId].rotationRate * 0.0065,
     equatorInclination: 0,
-    orbitalInclination: 17.1666,
+    orbitInclination: 17.1666,
     orbitCenter: solarSystemId,
     color: 'img/plutoColorMap.jpg',
     bump: 'img/plutoBumpMap.jpg',
@@ -182,12 +239,16 @@ data[plutoId] = {
 };
 data[moonId] = {
     name: "Moon",
-    orbitRate: 27.3,
+    diameter: "3474.2 km",
+    distanceFromEarth: "384400 km",
+    density: "3.34 g/cm\u00B3",
+    gravity: "1.62 m/s\u00B2",
+    revolutionRate: 27.3,
     rotationRate: data[earthId].rotationRate * 27.0321662037,
     distance: 2.0,
     size: data[earthId].size * 0.2725,
     equatorInclination: 0,
-    orbitalInclination: 5.25,
+    orbitInclination: 5.25,
     orbitCenter: earthSystemId,
     color: 'img/moonColorMap.jpg',
     bump: 'img/moonBumpMap.jpg',
@@ -204,7 +265,7 @@ data[asteroidBeltId] = {
     maxOffsetXZ: 30,
     number: 20000,
     orbitCenter: solarSystemId,
-    orbitRate: (data[jupiterId].orbitRate + data[marsId].orbitRate)/2,
+    revolutionRate: (data[jupiterId].revolutionRate + data[marsId].revolutionRate)/2,
     icon: 'img/asteroidBelt.png'
 };
 
@@ -526,7 +587,7 @@ function rotationMovement(Id) {
 
 function revolutionMovement(Id) {
     // Revolution angle
-    let theta = THREE.Math.degToRad(360) * (date.getTime() / (data[Id].orbitRate * 86400000)); // Cast revolutionRate in milliseconds
+    let theta = THREE.Math.degToRad(360) * (date.getTime() / (data[Id].revolutionRate * 86400000)); // Cast revolutionRate in milliseconds
 
     let currentId = Id;
     if (data[Id].hasOwnProperty('groupId')) currentId = data[Id].groupId;
@@ -624,7 +685,18 @@ function showInfoPlanet(event) {
 
         let currentId = targetElement.myId;
 
-        tooltipDiv.html("<img src='" + data[currentId].icon + "' width=20>" + " " + targetElement.name);
+        tooltipDiv.html("<div style='float:left;padding-top:10px;padding-left:10px;'><img src='" + data[currentId].icon + "' width=50></div> <h4 style='size:20pt;'>" + targetElement.name + "</h4>");
+        if(data[currentId].hasOwnProperty('rotationRate')) tooltipDiv.append("</br>Rotation: " + data[currentId].rotationRate.toFixed(2) + " hours");
+        if(data[currentId].hasOwnProperty('revolutionRate')) tooltipDiv.append("</br>Revolution: " + data[currentId].revolutionRate.toFixed(2) + " days");
+        if(data[currentId].hasOwnProperty('equatorInclination')) tooltipDiv.append("</br>Axis inclination: " + data[currentId].equatorInclination.toFixed(2) + "°");
+        if(data[currentId].hasOwnProperty('orbitInclination')) tooltipDiv.append("</br>Orbit inclination: " + data[currentId].orbitInclination.toFixed(2) + "°");
+        if(data[currentId].hasOwnProperty('type')) tooltipDiv.append("</br>Type: " + data[currentId].type);
+        if(data[currentId].hasOwnProperty('diameter')) tooltipDiv.append("</br>Diameter: " + data[currentId].diameter);
+        if(data[currentId].hasOwnProperty('averageDistanceFromSun')) tooltipDiv.append("</br>Average distance from sun: " + data[currentId].averageDistanceFromSun);
+        if(data[currentId].hasOwnProperty('distanceFromEarth')) tooltipDiv.append("</br>Distance from Earth: " + data[currentId].distanceFromEarth);
+        if(data[currentId].hasOwnProperty('density')) tooltipDiv.append("</br>Density: " + data[currentId].density);
+        if(data[currentId].hasOwnProperty('gravity')) tooltipDiv.append("</br>Gravity: " + data[currentId].gravity);
+        if(data[currentId].hasOwnProperty('numberOfMoons')) tooltipDiv.append("</br>Number of moons: " + data[currentId].numberOfMoons);
 
         setTimeout(function() {
             tooltipDiv.css({
@@ -936,7 +1008,7 @@ function render () {
     requestAnimationFrame(render);
     for (let i = sunId; i <= moonId; i++) movePlanet(i); // Animate planets
     if (play) incrementDate(); // Increment date
-    let theta = THREE.Math.degToRad(360) * (date.getTime() / (data[asteroidBeltId].orbitRate * 86400000)); // Asteroid belt rotation angle
+    let theta = THREE.Math.degToRad(360) * (date.getTime() / (data[asteroidBeltId].revolutionRate * 86400000)); // Asteroid belt rotation angle
     if (playRevolutionMovement) celestialObjects[asteroidBeltId].rotation.y = theta; // Rotate asteroid belt
     if (cameraFollowsPlanet) followPlanet(followPlanetId); // Follow target planet
     controls.update();
